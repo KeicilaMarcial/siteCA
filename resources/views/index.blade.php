1c3f9056eda96f782e565769575c009772988518
Luiz Guilherme <lgrm.606@gmail.com>
2019-06-12 15:34:31 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -93,15 +93,11 @@
             <div class="col-lg-12">
               <div class="banner_content text-center">
                 <p class="text-uppercase">
-                  Best online education service In the world
+                  CACCOMP
                 </p>
                 <h2 class="text-uppercase mt-4 mb-5">
-                  One Step Ahead This Season
+                  Centro Acadêmico de Ciência da Computação
                 </h2>
-                <div>
-                  <a href="#" class="primary-btn2 mb-3 mb-sm-0">learn more</a>
-                  <a href="#" class="primary-btn ml-sm-3 ml-0">see course</a>
-                </div>
               </div>
             </div>
           </div>
